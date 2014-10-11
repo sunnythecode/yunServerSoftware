a080a0c9b9ab37756fb587a99fa84b1e00633677
Daniel Grau <danielgrau@sfhs.com>
2014-10-11 14:41:40 -0700

    testing for errors

--- networkCom.cpp
+++ networkCom.cpp
@@ -1,9 +1,21 @@
-void sendString(char message[], struct client *clients)
+#include "networkCom.hpp"
+
+int sendString(char message[], struct client *clients)
 {
+	char* nullPos;
+	if(!(nullPos = strchr(message,'\0'))) return FORMATING_ERROR;
 	
 	while(clients!=NULL)
 	{
-		send(clients->socket, message, strlen(message),0);
+		int sendRet = send(clients->socket, message, strlen(message),0);
 		clients = client->nextClient;
+		if(sendRet == nullPos - message) printf("Send successful\n");
+		if(sendRet == EACCESS) printf("Access Denied\n");
 	}
+	return nullPos - message;
 }
+void recvString(struct client *client)
+{
+	recv(client->socket, client->buffer, MAX_BUFFER, 0);
+}
+
--- networkCom.hpp
+++ networkCom.hpp
@@ -1,3 +1,22 @@
-#define maxClients 5
+#define MAX_CLIENTS 5
+#define MAX_BUFF 100
+#define FORMATING_ERROR -1
+
+#include "structServClient.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <string.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h> 
+#include <net/if.h>
+
+
 
 void sendString(char message[], struct client *clientList);
+void recvString(struct client *client);
