b5bd228bb9f3bb74af974e5b0d18d460b908f8b7
zeroDecoder <zerodecoder@gmail.com>
2015-08-24 13:28:08 -0700

    changed josytick interface struct

--- soccerBotSoftware/joystickhandler.cpp
+++ soccerBotSoftware/joystickhandler.cpp
@@ -9,9 +9,41 @@ JoyStickHandler::JoyStickHandler()
 
     this->axisVal[0] = 0;
     this->buttonVal[0].bttns = 0xff;
-    this->buttonVal[0].indvBttn.bttn4 =false;
+    this->buttonVal[0].indvBttn.LEFT_THUMB =false;
+
+#if defined(__WIN32) || defined(__WIN64) || defined(__WINNT)
+    XInputEnable(FALSE); //start with all controllers disabled
+
+#elif __linux
+
+#elif __APPLE__
+
+#endif
+
 }
 
 JoyStickHandler::~JoyStickHandler()
 {
 }
+
+#if defined(__WIN32) || defined(__WIN64) || defined(__WINNT)
+void JoyStickHandler::initJoystick(int index)
+{
+
+    DWORD error=XInputGetState(index,&this->controller);
+    if(error!=1)
+    {
+
+    }
+}
+
+void JoyStickHandler::updateJoystick()
+{
+
+}
+
+#elif __linux
+
+#elif __APPLE__
+
+#endif
--- soccerBotSoftware/joystickhandler.h
+++ soccerBotSoftware/joystickhandler.h
@@ -16,16 +16,22 @@ typedef union
 {
   struct
   {
-    bool bttn1 : 1;
-    bool bttn2 : 1;
-    bool bttn3 : 1;
-    bool bttn4 : 1;
-    bool bttn5 : 1;
-    bool bttn6 : 1;
-    bool bttn7 : 1;
-    bool bttn8 : 1;
+    bool DPAD_UP        : 1;
+    bool DPAD_DOWN      : 1;
+    bool DPAD_LEFT      : 1;
+    bool DPAD_RIGHT     : 1;
+    bool START          : 1;
+    bool BACK           : 1;
+    bool LEFT_THUMB     : 1;
+    bool RIGHT_THUMB    : 1;
+    bool LEFT_SHOULDER  : 1;
+    bool RIGHT_SHOULDER : 1;
+    bool A              : 1;
+    bool B              : 1;
+    bool X              : 1;
+    bool Y              : 1;
   }indvBttn;
-  unsigned char bttns;
+  uint16_t bttns;
 }buttonArry;
 
 class JoyStickHandler
@@ -36,9 +42,24 @@ private:
    int buttonCount;
    buttonArry *buttonVal;
 
+#if defined(__WIN32) || defined(__WIN64) || defined(__WINNT)
+    int joy_dx_index;
+    XINPUT_STATE controller;
+#elif __linux
+
+#elif __APPLE__
+
+#endif
+
 public:
     JoyStickHandler();
     ~JoyStickHandler();
+    bool readBttn(int index);
+    int16_t readAxis(int index);
+    void initJoystick(int index);
+    void updateJoystick();
 };
 
 #endif // JOYSTICKHANDLER_H
+
+
