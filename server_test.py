0279d258363ce1a617c42319f04f032ae0f289ee
zeroDecoder <zerodecoder@gmail.com>
2015-09-08 17:34:15 -0700

    Broadcast reader

--- server_test.py
+++ server_test.py
@@ -5,27 +5,59 @@ from Queue import Queue
 from threading import Thread
  
 HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
+PORT = 2367 # Arbitrary non-privileged port
 data =' '
 BROADCAST_PORT = 400
-BROADCAST_IP = "127.0.0.1"
+BROADCAST_IP = '' # Symbolic name, meaning all available interfaces
+
+FLAG_ARDUINO_CONNECT = True
+KEEP_ALIVE_TIMEOUT = 3
+LOST_BROADCAST_CONNECTION = "1"
+
+def check4keepAlive(dgram):
+    a = dgram.split('.')
+    if len(a) != 4:           #check that the dgram is not too small
+        return False
+    for x in a:              #check that each member is a number less than 255
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i< -1 or i > 255:
+                return False
+    return True
 
 def broadcastListener():
+    fKeepAlive = True
+    lastKeepAlive = time.time()
+    
+    
     while True:
-        bCastSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        bCastSock =socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        bCastSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         bCastSock.bind((BROADCAST_IP, BROADCAST_PORT))
-        mainQueue.put("data")
-        
-        
+
+        while fKeepAlive:
+            data =  bCastSock.recv(1500) # buffer size is 1500 bytes
+            if check4keepAlive(data) == True:
+                lastKeepAlive = time.time()
+                broadcastQueue.put(data)
+            elif time.time() - lastKeepAlive > KEEP_ALIVE_TIMEOUT:
+                fKeepAlive = False
+                broadcastQueue.put(LOST_BROADCAST_CONNECTION)
+                
+            time.sleep(.1)
+        bCastSock.close()
+
+
+#start program
+mainQueue = Queue()
+broadcastQueue = Queue()
+
+t1 = Thread(target=broadcastListener, args=())
+t1.start()
 
 while  True:
-    mainQueue = Queue()
-    broadcastQueue = Queue()
-
-    t1 = Thread(target=broadcastListener, args=())
-    t1.start()
-    
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print 'New Socket created'
     #Bind socket to local host and port
@@ -33,41 +65,7 @@ while  True:
         s.bind((HOST, PORT))
     except socket.error as msg:
         print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        sys.exit()
+        
          
     print 'Socket bind complete'
-
-    #Start listening on socket
-    s.listen(10)
-    print 'Socket now listening'
-    #wait to accept a connection - this call is blocking
-    conn, addr= s.accept()                      
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    #now keep talking with the client
-    matchState = 0;
-    connState = True
-    while connState:
-            #loop back message from client
-            try:
-                data = conn.recv(1024)
-            except socket.error, (value,message):
-                if value == 10054: #if connection was reset remotely
-                    connState = False
-            try:
-                    delmPos = data.index('!')
-            except ValueError:
-                    print "could not find delim"
-                    connState = False
-            if len(data) > 1 :
-                    if data[0] == '1' :
-                            sendCnt =conn.send("lancer")
-                            print "number of chars sent: " + repr(sendCnt)   
-                    if data[0] == '6':
-                            matchState = 1 #matchstate 1 is match start, auto
-                            print "matchStart"
-                    if data[0] == '7':
-                            matchState = 2 #matchstate 2 is teleop
-                            print "teleop start"
-                    if data[0] == '8':
-                            matchState = 0 #matchstate 0 is matchEnd
-                            print "match over"
+    
