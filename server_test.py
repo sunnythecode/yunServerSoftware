1eb0730f96ab8ef1fa783e4405197946589c8bfc
Elis Pogace <zerodecoder@gmail.com>
2015-09-08 21:15:50 -0700

    Python bridge changes

--- server_test.py
+++ server_test.py
@@ -4,19 +4,16 @@ import time
 from Queue import Queue
 from threading import Thread
  
-HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 2367 # Arbitrary non-privileged port
-data =' '
-BROADCAST_PORT = 400
+PORT = 2367 # Game host port
+BROADCAST_PORT = 8900 # Broadcast port
 BROADCAST_IP = '' # Symbolic name, meaning all available interfaces
 
 FLAG_ARDUINO_CONNECT = True
 KEEP_ALIVE_TIMEOUT = 3
-LOST_BROADCAST_CONNECTION = "1"
-
+LOST_BROADCAST_CONNECTION = '127.0.0.1'
 def check4keepAlive(dgram):
     a = dgram.split('.')
-    if len(a) != 4:           #check that the dgram is not too small
+    if len(a) != 4:          #check that the dgram is not too small
         return False
     for x in a:              #check that each member is a number less than 255
         if not x.isdigit():
@@ -27,27 +24,26 @@ def check4keepAlive(dgram):
     return True
 
 def broadcastListener():
-    fKeepAlive = True
     lastKeepAlive = time.time()
-    
-    
-    while True:
-        bCastSock =socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        bCastSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    bCastSock =socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bCastSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    bCastSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    try:
         bCastSock.bind((BROADCAST_IP, BROADCAST_PORT))
+    except socket.error as msg:
+        print 'Broadcast bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
+     
 
-        while fKeepAlive:
-            data =  bCastSock.recv(1500) # buffer size is 1500 bytes
-            if check4keepAlive(data) == True:
-                lastKeepAlive = time.time()
-                broadcastQueue.put(data)
-            elif time.time() - lastKeepAlive > KEEP_ALIVE_TIMEOUT:
-                fKeepAlive = False
-                broadcastQueue.put(LOST_BROADCAST_CONNECTION)
-                
-            time.sleep(.1)
-        bCastSock.close()
-
+    while True:    
+        data =  bCastSock.recv(1500) # buffer size is 1500 bytes
+        if check4keepAlive(data) == True:
+            lastKeepAlive = time.time()
+            broadcastQueue.put(data)
+        elif time.time() - lastKeepAlive > KEEP_ALIVE_TIMEOUT:
+            broadcastQueue.put(LOST_BROADCAST_CONNECTION)
+        time.sleep(.1)
 
 #start program
 mainQueue = Queue()
@@ -57,15 +53,16 @@ t1 = Thread(target=broadcastListener, args=())
 t1.start()
 
 while  True:
+    #set up socket for connection to host
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print 'New Socket created'
-    #Bind socket to local host and port
+    print 'waiting for host broadcast.'
+    host = broadcastQueue.get(True)
+    print host
     try:
-        s.bind((HOST, PORT))
+        s.bind((host, PORT))
     except socket.error as msg:
         print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    
         
-         
-    print 'Socket bind complete'
     
