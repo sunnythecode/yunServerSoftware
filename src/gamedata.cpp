e60e26cc2915955d275caa55dbfe98e21849e75a
sameervijay <sameer.vijay4@gmail.com>
2015-09-26 15:22:29 -0700

    Revert "local stash"
    This reverts commit d0dd12e74d57c88620e0e9914db1f5d627f3a755.

--- src/gamedata.cpp
+++ src/gamedata.cpp
@@ -19,11 +19,16 @@ void GameData::endGameUpdates() {
     gameDataTimer.stop();
 }
 void GameData::updateGameData() {
+    this->gameTime += 0.1;
+
     // colon symbol after gmd, pound symbol between game time and everything else, semicolons between individual player data
     QTextStream allGameData;
     allGameData << "gmd:" << this->gameTime << "#";
     for (int i = 0; i < 6; i++) {
-            allGameData << "P" << i + 1 << ":" << this->robotNames[i] << ":"<< this->robotConnections[i] << ":" << this->joystickConnections[i]  << ";";
+        if (i == 0)
+            allGameData << "P" << i + 1 << ":" << this->robotNames[i] << ":" << this->joystickConnections[i] << ":" << this->clientConnections[i];
+        else
+            allGameData << ";P" << i + 1 << ":" << this->robotNames[i] << ":" << this->joystickConnections[i] << ":" << this->clientConnections[i];
     }
     this->gameSyncs++;
 
