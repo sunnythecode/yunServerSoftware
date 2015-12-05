1a203a367eb5d0348105101cb232458af33f60e6
DanG100 <daniel.d.j.grau@gmail.com>
2015-12-05 09:44:34 -0800

    Create installServer.sh

---
+++ src/installServer.sh
@@ -0,0 +1,32 @@
+#/bin/sh
+echo "::sysinit:/etc/init.d/rcS S boot
+::shutdown:/etc/init.d/rcS K shutdown
+#ttyATH0::askfirst:/bin/ash --login
+" > /etc/inittab
+
+opkg update
+opkg install python pyserial unzip
+mkdir /home
+mkdir /home/robotics
+cd /home/robotics
+wget https://github.com/Saint-Francis-Robotics-Team2367/yunServerSoftware/archive/jstickStatic.zip --no-check-certificate
+unzip jstickStatic.zip
+
+echo "# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+
+wifi-live-or-reset
+boot-complete-notify
+
+# Uncomment the following line in order to reset the microntroller
+# right after linux becomes ready
+
+#reset-mcu
+
+# Uncomment the following line in order to disable kernel console
+# debug messages, thus having a silent and clean serial communication
+# with the microcontroller
+
+#echo 0 > /proc/sys/kernel/printk
+python /home/robotics/yunServerSoftware-jstickStatic/src/server_test.py
+exit 0" > /etc/rc.local
