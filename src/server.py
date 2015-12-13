1002a7686de7f2112a3874d2de62ae482c537ba9
Elis Pogace <zerodecoder@gmail.com>
2015-12-12 17:00:21 -0800

    Big changes to python server. Seperate processes into three threads.

--- src/server.py
+++ src/server.py
@@ -1,72 +1,184 @@
 import socket
+import select
 import serial
+import string
 import sys
+import time
+import atexit
+from Queue import Queue
+from threading import Thread
  
-HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
-data =' '
-ser = serial.Serial('/dev/ttyATH0', 115200) # open serial port
-print ser.portstr
-ser.open()
-ser.flushInput()
+#network related variables
+PORT = 2380 # Game host port
+BROADCAST_PORT = 472 # Broadcast port
+BROADCAST_IP = '' 
+LOCAL_IP = ''
 
-while 1:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print 'New Socket created'
-    #Bind socket to local host and port
-    try:
-        s.bind((HOST, PORT))
-    except socket.error as msg:
-        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        sys.exit()
-         
-    print 'Socket bind complete'
+#timouts for respective interval timers
+KEEP_ALIVE_TIMEOUT = 2
+NAME_BROADCAST_TIMEOUT = 1
 
-    #Start listening on socket
-    s.listen(10)
-    print 'Socket now listening'
-    ser.write("!StartedstartstartedstartedSTARTED?")
-    #wait to accept a connection - this call is blocking
-    conn, addr= s.accept()                      
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    #now keep talking with the client
-    matchState = 0;
-    connState = True
-    while connState:
-            #loop back message from client
-            try:
-                data = conn.recv(1024)
-            except socket.error, (value,message):
-                if value == 10054: #if connection was reset remotely
-                    connState = False
-            try:
-                    delmPos = data.index('!')
-            except ValueError:
-                    print "could not find delim"
-                    connState = False
-            if len(data) > 1 :
-                    if data[0] == '1' :
-                            sendCnt =conn.send("lancer")
-                            print "number of chars sent: " + repr(sendCnt)   
-                    if data[0] == '6':
-                            matchState = 1 #matchstate 1 is match start, auto
-                            print "matchStart"
-                    if data[0] == '7':
-                            matchState = 2 #matchstate 2 is teleop
-                            print "teleop start"
-                    if data[0] == '8':
-                            matchState = 0 #matchstate 0 is matchEnd
-                            print "match over"
-                    if data[0] == '9' :
-                            try:
-                                    datagramLeng = ord(data[2])
-                            except ValueError:
-                                    datagramLeng = 0;
-                            if datagramLeng != len(data):
-                                    print data + " -Packet size mismatch: " + repr(datagramLeng)
-                            else:
-                                    ser.write(data)
-                                    #print data + " - " + str(len(data)) + ord(data[3])      
-    s.close()
+#thread communication queues
+broadcastQueue = Queue()
+serialRealTimeQueue = Queue()
+serialQueue = Queue()
+networkRealTimeQueue = Queue()
+networkQueue = Queue()
+
+#change this to the relevant team name when the script is loaded to the yun
+ROBOT_NAME = "ChangeThisToTheTeamName"
+
+#Error log file, overwtite file from last reset
+logFile = "./elog_" + str(time.time()) + ".txt"
+try:
+	log = open(logFile, "w")
+except Exception as f:
+	print "Uhhh... check that your hard drive isn't on fire!"   
+	sys.exit(1)
+else:
+	print "log File" + logFile
+
+#loging wrapper function    
+def logWrite(strng):
+	log.write("[" + str(time.time()) + "]" +strng +"\n")
+	print strng
+    
+#threaded function that listens for broadcasts from host.
+def broadcastListener():
+        
+	bCastSock =socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	bCastSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	bCastSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+	try:
+		bCastSock.bind((BROADCAST_IP, BROADCAST_PORT))
+	except socket.error as msg:
+		bCastSock.close()
+		logWrite('Broadcast bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1] +"\n")
+		sys.exit(1)
+			
+	lastKeepAlive = time.time()
+	while True:   
+		readReady = select.select([bCastSock], [], [])
+		if readReady[0]: 
+			data, sender =  bCastSock.recvfrom(1500) 
+            #check that the datagram is a alive packet
+			if dgram == source[0]:
+				lastKeepAlive = time.time()
+				broadcastQueue.put(sender)
+				nameQueue.put(sender)
+		time.sleep(.05)
+        
+#serial communication thread
+def serialComthread():
+	#setup arduino serial comm
+	try:
+		ser = serial.Serial('/dev/ttyATH0', 115200)
+	except Exception as msg:
+		if 'Windows' in str(msg):
+			noSerialFlag = True
+			logWrite("Running on windows, assuming debug mode. Printing serial data to STDOUT")
+		else:
+			noSerialFlag = False
+			logWrite("port in use by another program")
+			logWrite(msg[0])
+			sys.exit(1)
+	else:
+		print ser.portstr
+		ser.open()
+	
+	#thread main loop
+	while True:
+		#check for data that needs to be bridged to arduino
+		dataFlag = True
+		if not serialRealTimeQueue.empty():
+			data = serialRealTimeQueue.get
+		else:
+			dataFlag = False
+			
+		#if data was recieved send it to arduino
+		if dataFlag: 
+			if noSerialFlag:
+				print data
+			else:
+				ser.write(data)	
+        
+		#check if the arduino sent us some data to relay to the host
+		if not noSerialFlag:
+			readReady = select.select([ser], [], [])
+			if readReady[0]:
+				networkQueue.put(ser.read())
+			
+			
+def networkComThread():
+	#create socket to recieve from host
+	netSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	netSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	try:
+		netSock.bind((LOCAL_IP, PORT))
+	except socket.error as msg:
+		logWrite('Broadcast bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+		sys.exit(1)
+	
+	#main thead loop
+	while True:
+		#wait for data from broadcast listener thread
+		logWrite("Waiting for host.")
+		host = broadcastQueue.get(True)
+		
+		#run loop while we continue to get broadcast packets from the host
+		fKeepAlive = True
+		keepAliveTimer = time.time()
+		nameBroadcastTimer = time.time()
+		while fKeepAlive:
+			if not broadcastQueue.empty():
+				while not broadcastQueue.empty():
+					qPop = broadcastQueue.get()
+				keepAliveTimer = time.time() 
+			#if we haven't recieved a keep alive packet in a while kill the connection
+			if time.time() - keepAliveTimer > KEEP_ALIVE_TIMEOUT:
+				fKeepAlive = False
+				logWrite("Connection Died due to timeout")
+				break
+				
+			#check for data from host and pass it to the serial thread
+			ready = select.select([netSock], [], [])
+			if ready[0]:
+				bridgeData, senderAddr = netSock.recvfrom(1024)
+				if not bridgeData:
+					continue
+				else:
+					if senderAddr[0] == host[0]:
+						serialRealTimeQueue.put(bridgeData)    
+		
+			#check serial queue and pass to the host if data is available
+			if not networkQueue.empty():
+				serialData = networkQueue.get()
+				netSock.sendto('DBG:' + ROBOT_NAME + ':' + serialData,(host[0], PORT))
+				
+			
+				
+		
+		
+#cleanup function
+def cleanup():
+	logWrite('cleanup')
+	
+#Main program start
+serialThread = Thread(target=serialComthread, args=())
+broadcastThread = Thread(target=broadcastListener, args=())
+
+serialThread.start()
+broadcastThread.start()
+
+atexit.register(cleanup)
+while True:
+	if not serialThread.isAlive():
+		serialThread.run()
+		logWrite("Restarting serial thread")
+	if not broadcastThread.isAlive():
+		broadcastThread.run()
+		logWrite("Restarting broadcast listener thread")
+	time.sleep(1)
+	logWrite("uhhh..")
 	
