a56e56b625a44e4bc053e4a33aeef28cd148592b
Daniel Grau <DanG100@users.noreply.github.com>
2018-06-28 08:39:58 -0700

    use systemd for startup

--- src/installPiServer.sh
+++ src/installPiServer.sh
@@ -3,10 +3,29 @@ cd ~
 sudo apt update
 sudo apt install git python-smbus
 git clone https://github.com/Saint-Francis-Robotics-Team2367/yunServerSoftware.git
+
 echo "#!/bin/sh
-python /home/pi/yunServerSoftware/src/pi/server.py" > startServer.sh
+cd /home/pi/
+su pi -c 'python /home/pi/yunServerSoftware/src/pi/server.py" > startServer.sh
+
 chmod a+x startServer.sh
-echo "./startServer.sh" >> .bashrc
+
+echo "[Unit]
+Description=Soccer bots
+After=multi-user.target
+
+[Service]
+Type=idle
+ExecStart=/home/pi/startServer.sh
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /lib/systemd/system/soccerbots.service
+
+sudo chmod 644 /lib/systemd/system/soccerbots.service
+sudo systemctl daemon-reload
+sudo systemctl enable soccerbots.service
+
+
 echo -n "Enter Team Name? "
 read name < /dev/tty
 echo "[Main]
