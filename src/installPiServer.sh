c35b5303dd3de4c2b01f5a4ffa9700d8ebb94c14
Daniel Grau <DanG100@users.noreply.github.com>
2018-06-28 14:37:39 -0700

    more setup

--- src/installPiServer.sh
+++ src/installPiServer.sh
@@ -2,6 +2,8 @@
 cd ~
 sudo apt update
 sudo apt install git python-smbus
+sudo raspi-config nonint do_boot_behaviour B2
+
 git clone https://github.com/Saint-Francis-Robotics-Team2367/yunServerSoftware.git
 
 echo "#!/bin/sh
@@ -30,3 +32,7 @@ echo -n "Enter Team Name? "
 read name < /dev/tty
 echo "[Main]
 TeamName = $name" > /home/pi/yunServerSoftware/src/pi/config.ini
+sudo raspi-config nonint do_hostname $name
+
+echo "Rebooting"
+sudo reboot
